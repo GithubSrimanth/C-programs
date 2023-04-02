678d58de02465c6e714503e6c7a48d3f3d992786
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-04-02 11:31:38 +0530

    Create swap_two_array_ele.c

---
+++ Arrays/swap_two_array_ele.c
@@ -0,0 +1,31 @@
+/*C program to swap two array elements using pointers*/
+#include<stdio.h>
+int main()
+{
+int a[100],i,n,temp;
+printf("Enter array size: ");
+scanf("%d",&n);
+printf("Enter array elements: ");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);//a+i
+}
+temp = *(a+2);
+*(a+2)=*(a+5);
+*(a+5)=temp;
+printf("After swap array elements: ");
+for(i=0;i<n;i++)
+{
+printf("%d\t",a[i]);
+}
+}
+
+
+/* OUTPUT IS: $ gcc swaptwoarrayele.c -o stae
+
+user@DESKTOP-IL6OQ24 ~/Cprograms
+$ ./stae
+Enter array size: 6
+Enter array elements: 34 76 98 23 98 54
+After swap array elements: 34   76      54      23      98      98
+*/
