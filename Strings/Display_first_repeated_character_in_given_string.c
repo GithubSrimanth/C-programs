98fe84257e9a778b6e435618cc79c238a78c5120
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-04-09 19:16:40 +0530

    Create Display_first_repeated_character_in_given_string.c

---
+++ Strings/Display_first_repeated_character_in_given_string.c
@@ -0,0 +1,24 @@
+/*Find the first repeated character in the given string*/
+#include<stdio.h>
+void main()
+{
+char str[100];
+int i,n,c[123]={0};
+printf("Enter a string: ");
+gets(str);
+for(n=0;i<str[n]!='\0';n++);
+for(i=0;i<n;i++)
+{
+c[str[i]]++;
+if(c[str[i]]>1)
+{
+printf("%c",str[i]);
+break;
+}
+}
+}
+
+
+/*OUTPUT IS: Enter a string: Improve your skills
+o
+*/
