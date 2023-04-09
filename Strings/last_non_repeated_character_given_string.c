80029e7ed662618ffb870d5d4ea17b2fd8f45ead
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-04-09 22:37:01 +0530

    Create last_non_repeated_character_given_string.c

---
+++ Strings/last_non_repeated_character_given_string.c
@@ -0,0 +1,31 @@
+/*Find last non repeated character in the given string*/
+#include<stdio.h>
+void main()
+{
+char str[100];
+int temp,i,n,c[123]={0};
+printf("Enter a string: ");
+gets(str);
+for(n=0;str[n]!='\0';n++);
+for(i=0;i<n;i++)
+{
+c[str[i]]++;
+}
+for(i=0;i<n;i++)
+{
+if(c[str[i]]==1)
+{
+temp=str[i];
+}
+}
+printf("%c",temp);
+}
+
+
+/*OUTPUT IS: Enter a string: srimanth gaddam
+g
+user@DESKTOP-IL6OQ24 ~/Cprograms
+$ ./lrc
+Enter a string: srimanthsri
+h
+ */
