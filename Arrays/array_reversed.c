c8bdc4592dd1a374cce9db2b31efb1bbc230f56c
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-04-11 21:21:55 +0530

    Update array_reversed.c

--- Arrays/array_reversed.c
+++ Arrays/array_reversed.c
@@ -1,3 +1,4 @@
+/*Reverse values/elements of an array*/
 #include<stdio.h>
 int main()
 {
@@ -14,3 +15,12 @@ for(i=0;i<5;i++)
 printf("%d",*p--);
 }
 }
+
+
+/*OUTPUT IS: Enter array values for arr[0]9
+Enter array values for arr[1]3
+Enter array values for arr[2]5
+Enter array values for arr[3]2
+Enter array values for arr[4]7
+the reveresed array values are: 72539
+*/
