bd2a77ae43bd56170dee103684cda889a7ec55a7
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-05-26 10:27:17 +0530

    Create reverse_string_using_function.c

---
+++ Strings/reverse_string_using_function.c
@@ -0,0 +1,25 @@
+/*Reverse the given string using function in C*/
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+printf("Enter a string: ");
+gets(str);
+reverseString(str);
+printf("Reversed string: %s\n", str);
+}
+
+void reverseString(char str[])
+{
+    int i,j,n,temp;
+for(n=0;str[n]!='\0';n++);//n is length of the string
+for(i=0,j=n-1;i<j;i++,j--)
+{
+t=str[i];
+str[i]=str[j];
+str[j]=t;
+}
+}
+
+/*OUTPUT IS: */
