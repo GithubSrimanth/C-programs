ec727718581439e872239f1b279e4c21ccc894da
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2023-09-07 11:36:24 +0530

    Update convert_deci_to_octal.c

--- Arrays/convert_deci_to_octal.c
+++ Arrays/convert_deci_to_octal.c
@@ -2,18 +2,18 @@
 #include<stdio.h>
 void decTooct(int decnum)
 {
-    int binary[32];
+    int octal[32];
     int n=0;
     while(decnum>0)
     {
-        binary[n]=decnum%8;
+        octal[n]=decnum%8;
         decnum/=8;
         n++;
     }
     printf("After Octal representaion\n");
     for(int i=n-1;i>=0;i--)
     {
-        printf("%d",binary[i]);
+        printf("%d",octal[i]);
     }
     printf("\n");
 }
