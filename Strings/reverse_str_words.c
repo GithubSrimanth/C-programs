6e7a4fe0681af61171e8e04922778ce8b3100bcc
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2024-03-24 18:27:21 +0530

    Create reverse_str_words.c

---
+++ Strings/reverse_str_words.c
@@ -0,0 +1,36 @@
+// C Program to Reverse Order of Words in a String 
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  	char str[100];
+  	int i, len;
+ 
+  	printf("\n Please Enter any String :  ");
+  	gets(str);
+  	
+  	len = strlen(str);
+  	printf("\n After Reversed word by word then string is: \n"); 	   	
+  	for(i = len - 1; i >= 0; i--)
+	{
+		if(str[i] == ' ')
+		{
+			str[i] = '\0';
+			printf("%s ", &(str[i]) + 1);	
+		} 
+	}
+	printf("%s", str);
+	
+  	return 0;
+}
+
+/* OUTPUT IS:
+
+Please Enter any String :  My name is Gaddam Srimanth
+
+After Reversed word by word then string is: 
+Srimanth Gaddam is name My
+
+*/
