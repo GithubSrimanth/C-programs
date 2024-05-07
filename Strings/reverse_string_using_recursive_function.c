086affaa7cf19337471bda0d6f7c1c58d1d798d2
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2024-05-07 19:19:55 +0530

    Update reverse_string_using_recursive_function.c

--- Strings/reverse_string_using_recursive_function.c
+++ Strings/reverse_string_using_recursive_function.c
@@ -1,4 +1,30 @@
 /*Reverse the given string using recursive function */
+#include <stdio.h>
+
+// Function to reverse a string using recursion
+void reverseString(char *str) {
+    if (*str == '\0')
+        return; // Base case: if the string is empty, return
+    
+    reverseString(str + 1); // Recursively call the function with the next character
+    
+    printf("%c", *str); // Print the current character after all the recursive calls have returned
+}
+
+int main() {
+    char str[100];
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    printf("Reversed string: ");
+    reverseString(str);
+    printf("\n");
+
+    return 0;
+}
+
+#######################################################################################
 #include<stdio.h>
 #include<string.h>
 void reverse(char *a)//define the func reverse, here pass the array so, func parameter would be char pointer
