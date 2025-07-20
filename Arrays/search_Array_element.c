82b5581c7c2def739cec517431bb09b0d7e4d7e1
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2025-07-20 18:47:55 +0530

    Update search_Array_element.c

--- Arrays/search_Array_element.c
+++ Arrays/search_Array_element.c
@@ -1,27 +1,58 @@
-#include<stdio.h>
+//WAP for search a element in array
+#include <stdio.h>
 #define MAX_SIZE 100
+
 int main()
 {
-int size, i, se;
-int a[MAX_SIZE];
-printf("Enter array size: ");
-scanf("%d",&size);
-printf("Enter array ele's:");
-for(i=0;i<size;i++)
-{
-scanf("%d",&a[i]);
+    int size, i, se;
+    int a[MAX_SIZE];
+    int found = 0; // flag to check if element is found
+
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
+    printf("Enter array elements: ");
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter search element: ");
+    scanf("%d", &se);
+
+    for(i = 0; i < size; i++)
+    {
+        if(a[i] == se)
+        {
+            printf("Search element %d found at position %d\n", se, i);
+            found = 1;
+            break; // stop searching after finding
+        }
+    }
+
+    if(found==0)
+    {
+        printf("Search element %d is not found\n", se);
+    }
+
+    return 0;
 }
-printf("enter search element:");
-scanf("%d",&se);
-for(i=0;i<size;i++)
-{
-if(a[i]==se)
-{
-printf("search ele is %d found at position %d",se, i);
-}
-else
-{
-printf("search ele is not found");
-}
-}
-}
+
+OUTPUT:
+Enter array size: 5
+Enter array elements: 1
+2
+3
+4
+5
+Enter search element: 7
+Search element 7 is not found
+
+Enter array size: 5
+Enter array elements: 1
+2
+3
+4
+5
+Enter search element: 4
+Search element 4 found at position 3
