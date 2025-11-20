e76141100f362b73a7519a3dc6a73c408e241260
GithubSrimanth <129396458+GithubSrimanth@users.noreply.github.com>
2025-11-20 22:06:34 +0530

    Update Prime_numbers.c

--- Arrays/Prime_numbers.c
+++ Arrays/Prime_numbers.c
@@ -1,54 +1,47 @@
 // Print the prime numbers in  n number of integer values using c
-#include <stdio.h>
-
-// Function to check whether a number is prime or not
-int isPrime(int num) {
-    if (num <= 1)
-        return 0; // 0 and 1 are not prime numbers
-    
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0)
-            return 0; // if the number is divisible by any number other than 1 and itself, it's not prime
-    }
-    
-    return 1; // if the number is not divisible by any number other than 1 and itself, it's prime
-}
-
-int main() {
-    int arr[50],n;
-    printf("Enter the range: ");
-    scanf("%d", &n);
-printf("Enter numbers\n");
-for(int i=0;i<n;i++)
+#include<stdio.h>
+int isprime(int num)
 {
-    scanf("%d", &arr[i]);
-}
-    printf("Prime numbers up to %d are: ", n);
-    for (int i = 2; i <= n; i++) {
-        if (isPrime(i))
-            printf("%d ", i);
+    if(num<=1)
+    {
+        return 0;
     }
-    printf("\n");
+    for(int i=2;i*i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
-    return 0;
+void main()
+{
+    int r;
+    printf("Enter range of number: \n");
+    scanf("%d", &r);
+    
+    
+    printf("Prime numbers up to %d\n", r);
+    for(int i=2;i<=r;i++)
+    {
+        if(isprime(i))
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
 }
 
 OUTPUT:
-Enter the range: 10
-Enter numbers
-1
+Enter range of number: 
+10
+Prime numbers up to 10
 2
 3
-4
 5
-6
 7
-8
-9
-10
-Prime numbers up to 10 are: 2 3 5 7 
-
-
 
     #########################################################################################
     Another logics of Prime numbers
